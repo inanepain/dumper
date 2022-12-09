40fcd7e1e3c35cc960f1ca741724986b75671d12
Philip Michael Raab <peep@inane.co.za>
2022-12-09 16:58:50 +0200

    - new: `Dumper::assert` method - first argument true/false expression followed by usual dump arguments  - new: `da` global function shortcut for `Dumper::assert`  - new: parameter `$limit` for `Silence` => toggles state returned when limit reached  - many minor fixes & updates to code & documentation  - major performance improvements

--- src/Dumper.php
+++ src/Dumper.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
- * This file is part of the Inane suite of tools.
+ * Inane: Dumper
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * A little tool to help with debugging by writing a `var_dump` like message unobtrusively to a collapsible panel at the bottom of a page.
  *
  * PHP version 8.1
  *
  * @author Philip Michael Raab <peep@inane.co.za>
  * @package Inane\Dumper
+ * @category debug
  *
- * @license MIT
- * @license https://inane.co.za/license/MIT
+ * @license UNLICENSE
+ * @license https://github.com/inanepain/dumper/raw/develop/UNLICENSE UNLICENSE
  *
  * @version $Id$
  * $Date$
@@ -48,7 +48,8 @@ use const true;
 
 use Inane\Stdlib\{
     Parser\ObjectParser,
-    Highlight
+    Highlight,
+    Options
 };
 
 /**
@@ -56,7 +57,7 @@ use Inane\Stdlib\{
  *
  * A simple dump tool that neatly stacks its collapsed dumps on the bottom of the page.
  *
- * @version 1.9.1
+ * @version 1.10.0-dev
  *
  * @todo: move the two rendering methods into their own classes. allow for custom renderers.
  *
@@ -66,7 +67,7 @@ final class Dumper {
     /**
      * Dumper version
      */
-    public const VERSION = '1.9.1';
+    public const VERSION = '1.10.0-dev';
 
     /**
      * Single instance of Dumper
@@ -97,7 +98,11 @@ final class Dumper {
     ];
 
     /**
-     * Set to false to stop dumper writing to page. instant quiet.
+     * Enable or Disable Dumper
+     *
+     * true: normal functionality of Dumper
+     * false: stops dumper writing to page. instant quiet.
+     *
      * PS: this effect manual calls to write dumps as well.
      */
     public static bool $enabled = true;
@@ -112,7 +117,7 @@ final class Dumper {
     /**
      * Use php's var_export to generate dump
      *
-     * By default Dumper parses variables itself,
+     * default: Dumper parses variables itself,
      *  you can however have it use `var_export` instead
      *  by setting this to `true`
      *
@@ -128,6 +133,13 @@ final class Dumper {
      * Colours used for display
      */
     public static Highlight $highlight = Highlight::CURRENT;
+
+    /**
+     * Instantiated Silences
+     *
+     * @since 1.10.0
+     */
+    protected static Options $silences;
 
     /**
      * The collected dumps
@@ -147,6 +159,8 @@ final class Dumper {
      * Private Dumper constructor
      */
     private function __construct() {
+        // echo __METHOD__;
+        if (!isset(Dumper::$silences)) Dumper::$silences = new Options();
     }
 
     /**
@@ -160,22 +174,22 @@ final class Dumper {
      *
      * @param null|string $shortcut [optional] register a global variable shortcut function with name $shortcut. Shortcut must also be a valid variable/function name.
      *
-     * @return static
+     * @return \Inane\Dumper\Dumper
      */
-    public static function dumper(?string $shortcut = null): static {
-        if (!isset(static::$instance)) static::$instance = new static();
+    public static function dumper(?string $shortcut = null): Dumper {
+        if (!isset(Dumper::$instance)) Dumper::$instance = new Dumper();
 
         if (!is_null($shortcut) && !isset($GLOBALS[$shortcut]) && preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $shortcut))
-            $GLOBALS[$shortcut] = static::dump(...);
+            $GLOBALS[$shortcut] = Dumper::dump(...);
 
-        return static::$instance;
+        return Dumper::$instance;
     }
 
     /**
      * When destroyed the dumps get written to page
      */
     public function __destruct() {
-        if (static::$enabled && count(static::$dumps) > 0) {
+        if (Dumper::$enabled && count(Dumper::$dumps) > 0) {
             ob_start();
             // echo static::$instance->render();
             echo $this->render();
@@ -187,14 +201,17 @@ final class Dumper {
      * OR
      * Without Args: Write current dumps to page
      *
-     * @param mixed $data item to dump
+     * @param mixed       $data item to dump
      * @param null|string $label
-     * @param array $options
+     * @param array       $options
      *
-     * @return Dumper
+     * @return \Inane\Dumper\Dumper
+     *
+     * @throws \Inane\Stdlib\Exception\RuntimeException
+     * @throws \ReflectionException
      */
-    public function __invoke(mixed $data = null, ?string $label = null, array $options = []): static {
-        return static::dump($data, $label, $options);
+    public function __invoke(mixed $data = null, ?string $label = null, array $options = []): Dumper {
+        return Dumper::dump($data, $label, $options);
     }
 
     /**
@@ -202,29 +219,29 @@ final class Dumper {
      *
      * @return string
      */
-    protected function render(array $options = []): string {
+    protected function render(): string {
         // Check for command line
-        if (static::isCli()) {
-            $c = (object) static::$colour;
+        if (Dumper::isCli()) {
+            $c = (object) Dumper::$colour;
 
-            $code = implode("{$c->y}==========================================================================={$c->e} \n", static::$dumps);
-            static::$dumps = [];
-            return "{$c->m}DUMPER{$c->e}\n{$code}";
+            $code = implode("$c->y===========================================================================$c->e \n", Dumper::$dumps);
+            Dumper::$dumps = [];
+            return "{$c->m}DUMPER$c->e\n$code";
         }
 
-        $code = implode("\n", static::$dumps);
-        static::$dumps = [];
+        $code = implode("\n", Dumper::$dumps);
+        Dumper::$dumps = [];
 
         $style = file_get_contents(__DIR__ . '/../css/dumper.css');
 
-        $open = static::$expanded ? ' open' : '';
+        $open = Dumper::$expanded ? ' open' : '';
 
         return <<<DUMPER_HTML
-<style id="inane-dumper-style">{$style}</style>
+<style id="inane-dumper-style">$style</style>
 <div class="dumper">
-<details class="dumper-window"{$open}>
+<details class="dumper-window"$open>
     <summary class="dumper-title">dumper</summary>
-    <div class="dumper-body">{$code}</div>
+    <div class="dumper-body">$code</div>
 </details>
 </div>
 DUMPER_HTML;
@@ -255,54 +272,92 @@ DUMPER_HTML;
     }
 
     /**
+     * Get method silence state
+     *
+     * @since 1.10.0
+     *
+     * @param \Inane\Stdlib\Options $backtrace
+     *
+     * @return bool silence state
+     *
+     * @throws \Inane\Stdlib\Exception\RuntimeException
+     */
+    private static function getMethodSilence(Options $backtrace): bool {
+        $class = Dumper::$silences->get($backtrace->class)->object;
+        $instance = null;
+
+        if (Dumper::$silences->get($backtrace->class)->methods->has($backtrace->function))
+            $instance = Dumper::$silences->get($backtrace->class)->methods->get($backtrace->function)->instance;
+        else if ($backtrace->function) {
+            $attributes = $class->getMethod($backtrace->function)->getAttributes(Silence::class);
+            $instance = count($attributes) > 0 ? $attributes[0]->newInstance() : null;
+
+            Dumper::$silences->get($backtrace->class)->methods->set($backtrace->function, [
+                'instance' => $instance,
+            ]);
+        }
+
+        return $instance && $instance();
+    }
+
+    /**
      * Create a label for the dump with relevant information
      *
      * @param string|null $label
+     * @param string|null $type
      *
      * @return string|null If Attribute Silence true return null
+     *
+     * @throws \Inane\Stdlib\Exception\RuntimeException
+     * @throws \ReflectionException
      */
     protected static function formatLabel(?string $label = null, string $type = null): ?string {
-        [$src, $obj] = static::getTrace();
+        [$src, $obj] = Dumper::getTrace();
 
-        $data = [];
-        $data['file'] = $src['file'] ?? '';
-        $data['line'] = $src['line'] ?? '';
+        $data = new Options();
+        $data->file = $src['file'] ?? '';
+        $data->line = $src['line'] ?? '';
 
         if (!is_null($obj)) {
-            $data['class'] = $obj['class'] ?? false;
-            $data['function'] = $obj['function'] ?? '';
-        } else $data['class'] = false;
+            $data->class = $obj['class'] ?? false;
+            $data->function = $obj['function'] ?? '';
+        } else $data->class = false;
 
         // checking classes/functions for Silence attribute
-        if ($data['class'] != false) {
-            $class = new ReflectionClass($data['class']);
-            $attributes = $class->getAttributes(Silence::class);
-            if (count($attributes) > 0 && ($attributes[0]->newInstance())()) return null;
+        if ($data->class) {
+            if (Dumper::$silences->has($data->class)) {
+                $classInstance = Dumper::$silences->get($data->class)->instance;
+            } else {
+                $class = new ReflectionClass($data->class);
+                $attributes = $class->getAttributes(Silence::class);
+                $classInstance = count($attributes) > 0 ? $attributes[0]->newInstance() : null;
 
-            if ($data['function'] != false) {
-                $method = $class->getMethod($data['function']);
-                $attribs = $method->getAttributes(Silence::class);
-                if (count($attribs) > 0 && ($attribs[0]->newInstance())()) return null;
+                Dumper::$silences->set($data->class, [
+                    'object' => $class,
+                    'instance' => $classInstance,
+                    'methods' => [],
+                ]);
             }
+            if (($classInstance && $classInstance()) || Dumper::getMethodSilence($data)) return null;
         }
 
-        $label = isset($label) ? "{$label} [{$type}]" : $type;
+        $label = isset($label) ? "$label [$type]" : $type;
 
         // CHECK CONSOLE
-        if (static::isCli()) {
-            $c = (object) static::$colour;
+        if (Dumper::isCli()) {
+            $c = (object) Dumper::$colour;
 
-            $title = isset($label) ? "{$c->b} {$label}:{$c->e} " : '';
-            $file = "{$c->w}{$data['file']}{$c->e}::{$c->r}{$data['line']}{$c->e}";
-            $class = $data['class'] ? " => {$c->y}{$data['class']}::{$data['function']}{$c->e}" : '';
+            $title = isset($label) ? "$c->b $label:$c->e " : '';
+            $file = "$c->w$data->file$c->e::$c->r$data->line$c->e";
+            $class = $data->class ? " => $c->y$data->class::$data->function$c->e" : '';
         } else {
             // HTML
-            $title = isset($label) ? "<strong class=\"dump-label\">{$label}</strong> " : '';
-            $file = "{$data['file']}::<strong>{$data['line']}</strong>";
-            $class = $data['class'] ? " => {$data['class']}::<strong>{$data['function']}</strong>" : '';
+            $title = isset($label) ? "<strong class=\"dump-label\">$label</strong> " : '';
+            $file = "$data->file::<strong>$data->line</strong>";
+            $class = $data->class ? " => $data->class::<strong>$data->function</strong>" : '';
         }
 
-        return "{$title}{$file}{$class}" . PHP_EOL;
+        return "$title$file$class" . PHP_EOL;
     }
 
     /**
@@ -314,12 +369,12 @@ DUMPER_HTML;
      *
      * @return array info
      */
-    protected static function analyseVariable($v): array {
-        $trace = static::getTrace()[0];
+    protected static function analyseVariable(mixed $v): array {
+        $trace = Dumper::getTrace()[0];
         $file = file($trace['file']);
         $id = $trace['line'] - 1;
         $line = $file[$id];
-        preg_match('/(?:\()(\\$(\w+))/', $line, $match);
+        preg_match('/\((\\$(\w+))/', $line, $match);
 
         if (count($match) == 0) $match = ['', ''];
         else if (count($match) == 3) array_shift($match);
@@ -341,33 +396,33 @@ DUMPER_HTML;
      */
     protected function addDump(mixed $data, ?string $label = null, array $options = []): void {
         // Parse the variable to string
-        $code = ($options['useVarExport'] ?? static::$useVarExport) ? var_export($data, true) : ObjectParser::parse($data);
+        $code = ($options['useVarExport'] ?? Dumper::$useVarExport) ? var_export($data, true) : ObjectParser::parse($data);
 
-        $bufferMessage = static::$bufferOutput;
+        $bufferMessage = Dumper::$bufferOutput;
 
         // CHECK CONSOLE
-        if (static::isCli()) $output = "{$label}{$code}" . PHP_EOL;
+        if (Dumper::isCli()) $output = "$label$code" . PHP_EOL;
         else {
             // HTML
-            $highlight = $options['highlight'] ?? static::$highlight;
+            $highlight = $options['highlight'] ?? Dumper::$highlight;
             $code = $highlight->render($code);
 
             $open = ($options['open'] ?? false) ? 'open' : '';
 
             $output = <<<DUMPER_HTML
 <div class="dump">
-<details class="dump-window"{$open}>
-    <summary>{$label}</summary>
-    <code>{$code}</code>
+<details class="dump-window"$open>
+    <summary>$label</summary>
+    <code>$code</code>
 </details>
 </div>
 DUMPER_HTML;
         }
 
-        if ($bufferMessage) static::$dumps[] = $output;
+        if ($bufferMessage) Dumper::$dumps[] = $output;
         else {
-            $c = (object) static::$colour;
-            echo "\t\t{$c->m}DUMPER{$c->e}:{$output}" . PHP_EOL;
+            $c = (object) Dumper::$colour;
+            echo "\t\t{$c->m}DUMPER$c->e:$output" . PHP_EOL;
         }
     }
 
@@ -381,19 +436,50 @@ DUMPER_HTML;
      * Chaining: You only need bracket your arguments for repeated dumps.
      * Dumper::dump('one')('two', 'Label')
      *
-     * @param mixed $data item to dump
+     * @param mixed       $data item to dump
      * @param null|string $label
-     * @param array $options
+     * @param array       $options
      *
-     * @return Dumper
+     * @return \Inane\Dumper\Dumper
+     *
+     * @throws \Inane\Stdlib\Exception\RuntimeException
+     * @throws \ReflectionException
      */
-    public static function dump(mixed $data = null, ?string $label = null, array $options = []): static {
-        $info = static::analyseVariable($data);
+    public static function dump(mixed $data = null, ?string $label = null, array $options = []): Dumper {
+        $info = Dumper::analyseVariable($data);
         if (is_null($label) && $info['variable'] != '') $label = $info['variable'];
 
-        $label = static::formatLabel($label, $info['type']);
-        if (!is_null($label)) static::dumper()->addDump($data, $label, $options);
+        $label = Dumper::formatLabel($label, $info['type']);
+        if (!is_null($label)) Dumper::dumper()->addDump($data, $label, $options);
 
-        return static::dumper();
+        return Dumper::dumper();
+    }
+
+    /**
+     * Conditionally adds a dump to the collection
+     *
+     * options:
+     *  - (bool=false) open        : true - creates dumps open (main panel not effect)
+     *  - (bool=false) useVarExport: true - uses `var_export` instead of dumper to generate dump string
+     *
+     * Chaining: You only need bracket your arguments for repeated dumps.
+     * Dumper::dump('one')('two', 'Label')
+     *
+     * @since 1.10.0
+     *
+     * @param bool        $expression true suppress dump, false dump $data
+     * @param mixed       $data       item to dump
+     * @param null|string $label
+     * @param array       $options
+     *
+     * @return \Inane\Dumper\Dumper
+     *
+     * @throws \Inane\Stdlib\Exception\RuntimeException
+     * @throws \ReflectionException
+     */
+    public static function assert(bool $expression, mixed $data = null, ?string $label = null, array $options = []): Dumper {
+        if (!$expression) return Dumper::dump($data, $label, $options);
+
+        return Dumper::dumper();
     }
 }
